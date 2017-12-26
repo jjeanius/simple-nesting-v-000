c6bbcde92d3be84a2a2e93d82c2525f1b9940761
Judy McGoldrick <jjeanius@verizon.net>
2017-12-26 19:38:41 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -102,6 +102,7 @@ def changing_alan
      }
 
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.alans_new_info = "GUI"
+		 binding.pry
        programmer_hash [:alan_kay] [:alans_new_info] = "GUI"
 
 			 #puts programmer_hash [:alans_kay]
