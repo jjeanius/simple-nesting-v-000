700c5f5c6dc29f5e42bfbb530f56db7df4714242
Judy McGoldrick <jjeanius@verizon.net>
2017-12-26 19:45:04 +0000

    automatically backed up by learn

--- spec/nested_spec.rb
+++ spec/nested_spec.rb
@@ -29,6 +29,7 @@ describe "nested hash" do
 
   describe "#changing_alan" do 
     it "operates on the programmer_hash and changes what Alan Kay is known for, returning the newly-changed hash" do 
+      binding.pry
       expect(changing_alan[:alan_kay][:known_for]).to eq("GUI")
     end
   end
